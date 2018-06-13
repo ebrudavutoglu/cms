1550c07e32dbd4b07aff80803495abbe68317606
Ebru Davutoglu <ebrudavutoglu@gmail.com>
2018-06-13 16:27:07 +0300

    Urunler liste icin htmlsayfasi olusturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,51 @@
-urun listesi content...
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card margin-top">
+                    <div class="card-header">
+                        <h3 class="card-title">Ürün Listesi</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped table-hover">
+                            <thead>
+                                <th>id</th>
+                                <th>url</th>
+                                <th>Başlık</th>
+                                <th>Açıklama</th>
+                                <th>Durum</th>
+                                <th>İşlem</th>
+
+                            </thead>
+                            <tbody>
+                                <td>1</td>
+                                <td>Monitor-askisi</td>
+                                <td>Monitör Askısı</td>
+                                <td>360derece kullanılabilen monitor askısıdır.</td>
+                                <td></td>
+                                <td><a href="#" class="btn  btn-outline-danger btn-sm"><i class="fa fa-trash-o"></i>Sil</a><a class="btn  btn-outline-info btn-sm" href="#"><i class="fa fa-pencil-square-o"></i>Düzenle</a></td>
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        <ul class="pagination pagination-sm m-0 float-right">
+                            <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+</section>
