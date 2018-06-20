92c23380933515953b660e3d4dcaa1b8f26cd6f5
Ebru Davutoglu <ebrudavutoglu@gmail.com>
2018-06-20 12:36:11 +0300

    page_style aktif hale getirildi

--- panel/application/views/includes/head.php
+++ panel/application/views/includes/head.php
@@ -7,4 +7,5 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php $this->load->view('includes/include_style'); ?>
+  <?php $this->load->view("{$viewFolder}/{$subViewFolder}/page_style"); ?>
 </head>
